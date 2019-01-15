85f99a9c2d9738fcd17285bd14a06c36501cc80b
RayZhou <ray.zhou@scmp.com>
2019-01-15 13:04:58 +0800

    Updated to handle request from different subjects

--- routes/index.js
+++ routes/index.js
@@ -16,16 +16,28 @@ router.get('/', function(req, res, next) {
 let qindex = 0;
 
 /* GET home page. */
-router.get('/getquestion', function (req, res, next) {
+router.get('/getquestion/subject/:subject/type/:type', function (req, res, next) {
+
+  var type = req.params.type;
+  var subject = req.params.subject;
+  var limit = req.params.limit;
+
   var fs = require('fs');
-  fs.readFile('data/chinese.json', 'utf8', function (err, data) {
+  fs.readFile(`data/${subject}.json`, 'utf8', function (err, data) {
     if (err) throw err;
     var json = JSON.parse(data);
 
+    // console.log(json);
+    let output = json.questions.filter(question => { 
+      // console.log(question.type);
+      return question.type == type;
+    });
+    // console.log(output);
+    if (qindex >= output.length) qindex = 0;
+
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.send(json.questions[qindex]);
+    res.send(output[qindex]);
     qindex++;
-    if (qindex >= json.questions.length) qindex = 0;
     // res.send(json.questions[Math.floor(Math.random() * json.questions.length)]);
   });
 });
